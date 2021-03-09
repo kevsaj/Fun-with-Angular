f26a5c775934d7ce0213aaba4abf694d940675ec
Kevin Sajan <kevsaj@gmail.com>
2021-03-08 22:51:18 -0500

    finding player one

--- myapp/src/app/board/board.component.ts
+++ myapp/src/app/board/board.component.ts
@@ -13,6 +13,24 @@ export class BoardComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.newGame();
+  }
+
+  newGame() {
+    this.squares = Array(9).fill(null);
+    this.winner = null;
+    this.xIsNext = true;
+  }
+
+  get player() {
+    return this.xIsNext ? 'X' : 'O';
+  }
+
+  makeMove(idx: number) {
+    if (!this.squares[idx]) {
+      this.squares.splice(idx, 1, this.player);
+      this.xIsNext = !this.xIsNext;
+    }
   }
 
 }
