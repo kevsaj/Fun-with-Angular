dda446cd94206928e596ef3ee8e48a56171b459c
Kevin Sajan <kevsaj@gmail.com>
2021-03-08 23:17:19 -0500

    fixed error on board

--- myapp/src/app/board/board.component.ts
+++ myapp/src/app/board/board.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./board.component.scss']
 })
 export class BoardComponent implements OnInit {
-  squares: any[];
+  squares: string[];
   xIsNext: boolean;
   winner: string;
 
